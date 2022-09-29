fa688aab814f762407927612bd33c45d23309e0c
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2022-09-29 23:40:17 +0300

    Replace `wget` with `curl`
    Git Bash for Windows has curl unlike wget

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/a/24716445
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/git-prompt.sh
 export OLD_FILES_PATH=~/old_files
 mkdir $OLD_FILES_PATH
 [ ! -f ~/.bashrc ] || mv ~/.bashrc $OLD_FILES_PATH
